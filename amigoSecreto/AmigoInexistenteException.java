a2afb6729345f9e8a6e45ccf5be7f87bf211835b
Kauã Vidal de Souza <167227277+Kaua-Vidal@users.noreply.github.com>
2024-07-25 11:40:58 -0300

    Delete amigoSecreto directory

--- amigoSecreto/AmigoInexistenteException.java
+++
@@ -1,7 +0,0 @@
-package dcx.ufpb.br.poo.amigoSecreto;
-
-public class AmigoInexistenteException extends Exception{
-    public AmigoInexistenteException(String e){
-        super(e);
-    }
-}
--- amigoSecreto/AmigoNaoSorteadoException.java
+++
@@ -1,7 +0,0 @@
-package dcx.ufpb.br.poo.amigoSecreto;
-
-public class AmigoNaoSorteadoException extends Exception{
-    public AmigoNaoSorteadoException(String e){
-        super(e);
-    }
-}
